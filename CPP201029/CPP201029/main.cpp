e84be1ecd7516ba4744c764b0fcc047abbb3b031
myl <myldast1@126.com>
2020-10-29 18:00:11 +0800

    hello world

---
+++ CPP201029/CPP201029/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CPP201029
+//
+//  Created by LongMa on 2020/10/29.
+//  Copyright © 2020 ht. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cout << "wow\n";
+    return 0;
+}
